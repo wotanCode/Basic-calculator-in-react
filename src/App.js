074b1a2263804600e1cc6b9053200436dd1f31cb
Pedro Yanez <pedro_y91@outlook.com>
2021-12-26 15:50:10 -0300

    feat/refactory del proyecto y finalizado

--- src/App.js
+++ src/App.js
@@ -1,209 +1,92 @@
 import "./App.css";
 import React, { useState } from "react";
 
-function App() {
-  const [pantalla, setPantalla] = useState("0");
-  const [sumar, setSumar] = useState(false);
-  const [restar, setRestar] = useState(false);
-  const [dividir, setDividir] = useState(false);
-  const [multiplicar, setMultiplicar] = useState(false);
-  const [resultado, setResultado] = useState("0");
 
-  // Operadores aritmeticos
-  const sumarfunc = () => {
-    if (pantalla !== 0 && sumar === false) {
-      setResultado(resultado + pantalla);
-      setSumar(true);
-      setPantalla("0");
-    } else if (pantalla !== 0 && sumar === true) {
-      funcResultado();
+
+function App() {
+  //Manejadores de estado
+  const [number1, setNumber1] = useState("");
+  const [number2, setNumber2] = useState("");
+  const [currentOperation, setCurrentOperation] = useState("");
+  const [result, setResult] = useState(0);
+
+  //Asignar operacion
+  const clickNumber = (val) => {
+    if (currentOperation === "") {
+      setNumber1(number1 + val);
+    } else {
+      setNumber2(number2 + val);
     }
-  };
+  }
+
+  //Asignar Operando
+  const clickOperation = (val) => {
+    setCurrentOperation(val)
+  }
 
   //Resultado
-  const funcResultado = () => {
-    setPantalla(resultado);
-    setSumar(false);
-  };
+  function getResult() {
+    switch (currentOperation) {
+      case "+":
+        setResult(Number(number1) + Number(number2));
+        break;
+      case "-":
+        setResult(Number(number1) - Number(number2));
+        break;
+      case "x":
+        setResult(Number(number1) * Number(number2));
+        break;
+      case "÷":
+        setResult(Number(number1) / Number(number2));
+        break;
+      default:
+        console.log("error al enviar el resultado");
+    }
+  }
+  //Limpiando todos los datos
+  const allClear = () => {
+    setNumber1("");
+    setNumber2("");
+    setCurrentOperation("");
+    setResult("");
+  }
 
-  // Reinicar pantalla
-  const reiniciar = () => {
-    setPantalla("0");
-  };
-
-  //Los numeros que estan en pantalla se cambian
-  const handlerpantalla = (e) => {
-    setPantalla(e.target.value);
-  };
-
-  // Cargar numeros a la pantalla
-  const num0 = () => {
-    if (pantalla !== "0") setPantalla(pantalla + "0");
-  };
-  const num7 = () => {
-    if (pantalla === "0") setPantalla(7);
-    else setPantalla(pantalla + "7");
-  };
-  const num8 = () => {
-    if (pantalla === "0") setPantalla(8);
-    else setPantalla(pantalla + "8");
-  };
-  const num9 = () => {
-    if (pantalla === "0") setPantalla(9);
-    else setPantalla(pantalla + "9");
-  };
-  const num4 = () => {
-    if (pantalla === "0") setPantalla(4);
-    else setPantalla(pantalla + "4");
-  };
-  const num5 = () => {
-    if (pantalla === "0") setPantalla(5);
-    else setPantalla(pantalla + "5");
-  };
-  const num6 = () => {
-    if (pantalla === "0") setPantalla(6);
-    else setPantalla(pantalla + "6");
-  };
-  const num1 = () => {
-    if (pantalla === "0") setPantalla(1);
-    else setPantalla(pantalla + "1");
-  };
-  const num2 = () => {
-    if (pantalla === "0") setPantalla(2);
-    else setPantalla(pantalla + "2");
-  };
-  const num3 = () => {
-    if (pantalla === "0") setPantalla(3);
-    else setPantalla(pantalla + "3");
-  };
+  //Limpiando El dato actual
+  const deleteNumber = () => {
+    if (number2 === "") {
+      setNumber1("")
+    } else if (number2 !== "") {
+      setNumber2("")
+    }
+  }
 
   return (
     <div className="App">
       <header className="">Calculadora wotanMaxpro-3000</header>
-      <main className="calculadora-body">
-        {/* pantalla de la calculadora */}
-        <div>
-          <input
-            onChange={(e) => {
-              handlerpantalla(e);
-            }}
-            value={pantalla}
-          />
+      {/* calculadora */}
+      <main className="calculator-grid">
+        <div className="output">
+          <div className="previous-operand">{currentOperation ? (number1 + currentOperation + number2) : ""}</div>
+          <div className="current-operand">{result ? result : (!currentOperation ? number1 : number2)}</div>
         </div>
-
-        {/* Botones de la calculadora */}
-        <div className="containerButtons">
-          <div>
-            <button
-              onClick={() => {
-                num7();
-              }}
-              className="Buttons numeros"
-            >
-              7
-            </button>
-            <button
-              onClick={() => {
-                num8();
-              }}
-              className="Buttons numeros"
-            >
-              8
-            </button>
-            <button
-              onClick={() => {
-                num9();
-              }}
-              className="Buttons numeros"
-            >
-              9
-            </button>
-            <button
-              onClick={() => {
-                reiniciar();
-              }}
-              className="Buttons menues"
-            >
-              C
-            </button>
-            <button
-              onClick={() => {
-                num4();
-              }}
-              className="Buttons numeros"
-            >
-              4
-            </button>
-            <button
-              onClick={() => {
-                num5();
-              }}
-              className="Buttons numeros"
-            >
-              5
-            </button>
-            <button
-              onClick={() => {
-                num6();
-              }}
-              className="Buttons numeros"
-            >
-              6
-            </button>
-            <button className="Buttons menues">÷</button>
-            <button
-              onClick={() => {
-                num1();
-              }}
-              className="Buttons numeros"
-            >
-              1
-            </button>
-            <button
-              onClick={() => {
-                num2();
-              }}
-              className="Buttons numeros"
-            >
-              2
-            </button>
-            <button
-              onClick={() => {
-                num3();
-              }}
-              className="Buttons numeros"
-            >
-              3
-            </button>
-            <button className="Buttons menues">X</button>
-            <button
-              onClick={() => {
-                num0();
-              }}
-              className="Buttons numeros"
-            >
-              0
-            </button>
-            <button className="Buttons numeros">.</button>
-            <button
-              onClick={() => {
-                sumarfunc();
-              }}
-              className="Buttons menues"
-            >
-              +
-            </button>
-            <button className="Buttons menues">-</button>
-            <button
-              onClick={() => {
-                funcResultado();
-              }}
-              className="Buttons igual"
-            >
-              =
-            </button>
-          </div>
-        </div>
+        <button onClick={allClear} className="span-two">AC</button>
+        <button onClick={deleteNumber}>DEL</button>
+        <button onClick={() => { clickOperation("÷") }}>÷</button>
+        <button onClick={() => { clickNumber(7) }}>7</button>
+        <button onClick={() => { clickNumber(8) }}>8</button>
+        <button onClick={() => { clickNumber(9) }}>9</button>
+        <button onClick={() => { clickOperation("x") }}>x</button>
+        <button onClick={() => { clickNumber(4) }}>4</button>
+        <button onClick={() => { clickNumber(5) }}>5</button>
+        <button onClick={() => { clickNumber(6) }}>6</button>
+        <button onClick={() => { clickOperation("+") }}>+</button>
+        <button onClick={() => { clickNumber(1) }}>1</button>
+        <button onClick={() => { clickNumber(2) }}>2</button>
+        <button onClick={() => { clickNumber(3) }}>3</button>
+        <button onClick={() => { clickOperation("-") }}>-</button>
+        <button onClick={() => { clickNumber(".") }}>.</button>
+        <button onClick={() => { clickNumber(0) }}>0</button>
+        <button onClick={getResult} className="span-two">=</button>
       </main>
     </div>
   );
